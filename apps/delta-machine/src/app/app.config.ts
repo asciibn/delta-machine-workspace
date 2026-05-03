4b5856deefdfd995190b58623ff20fc67e8430fa
Russ Ferguson <rferguson@technologycoach.com>
2026-05-03 18:40:31 -0400

    added auth0

--- apps/delta-machine/src/app/app.config.ts
+++ apps/delta-machine/src/app/app.config.ts
@@ -4,7 +4,19 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import * as core from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [
+    (core as any).provideBrowserGlobalErrorListeners?.() || [], // Conditional for Angular v19
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(appRoutes),
+    provideAuth0({
+      domain: "dev-luxlocations.us.auth0.com",
+      clientId: "OOm4rGznLVehZLHtOJzRMaxJ4Faxajnp",
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+  },)],
 };
--- apps/delta-machine/src/app/app.ts
+++ apps/delta-machine/src/app/app.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterOutlet} from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [
+
+    RouterOutlet,
+    CommonModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected title = 'delta-machine';
+  protected readonly window = window;
+  protected auth = inject(AuthService);
 }
